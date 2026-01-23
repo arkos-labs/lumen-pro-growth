6845647fb5b942fae671f175afe37f840449b840
arkos <cherkinicolas@gmail.com>
2026-01-23 17:55:36 +0100

    Activation du nouveau template EmailJS (template_qcf2bqr) pour la Landing Page

--- src/config/email.ts
+++ src/config/email.ts
@@ -5,5 +5,5 @@ export const EMAIL_CONFIG = {
     PUBLIC_KEY: "0-T-0-9LG_-Q2_SuC",
     TO_EMAIL: "cherkinicolas@gmail.com",
     // Créez un nouveau template EmailJS et mettez son ID ici pour la Landing Page
-    LANDING_TEMPLATE_ID: "template_mvrjb45", // Je laisse l'ancien en attendant le nouveau
+    LANDING_TEMPLATE_ID: "template_qcf2bqr",
 };
